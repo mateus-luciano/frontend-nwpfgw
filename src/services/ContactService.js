2b7dc49e3f64414834d8bde22f903c323f7f6919
Mateus Luciano <teeusdm@gmail.com>
2021-03-19 09:27:22 -0300

    add mask in form phone

--- src/services/ContactService.js
+++ src/services/ContactService.js
@@ -6,13 +6,28 @@ class ContactService extends BaseServer {
         super()
         this.name = nameContact
         this.email = email
-        this.phone = phone
+        this.phone = er4
         this.message = message
     }
 
     init() {
         this.assignEventSendNewContact()
+        this.addMaskInPhone()
     }
+
+    
+    addMaskInPhone() {
+        let number = document.getElementById('er4')
+        let maskNumber = ''
+        number.addEventListener('keyup', ()=> {
+            if (number.value.length === 2) {
+                number.value = `(${number.value}) `
+                let maskNumber  = number.value
+            } else if (number.value.length === 10) {
+                number.value = `${maskNumber} ${number.value}-`
+            }
+        })
+	}
 
     assignEventSendNewContact() {
         document.getElementsByClassName('btn-send-contact')[0].addEventListener('click', event => {
